2ff508f6c23c930c2042c5313c4e319fb04c6678
Angela Ferrer <gelferrero01@gmail.com>
2024-02-09 20:18:52 +0800

    converting landmarks, error determined

--- final_project.py
+++ final_project.py
@@ -18,9 +18,10 @@ mp_hands = mp.solutions.hands
 # Encoding gestures to be recognized by the tracker
 class Gest(IntEnum):
     # Binary Encoded
-     """
+    """
     Enum for mapping all hand gesture to binary number.
     """
+    
     FIST = 0
     PINKY = 1
     RING = 2
@@ -43,3 +44,95 @@ class HLabel(IntEnum):
     MINOR = 0
     MAJOR = 1
 
+# Convert Mediapipe Landmarks to recognizable Gestures
+class HandRecog:
+    """
+    Convert Mediapipe Landmarks to recognizable Gestures.
+    """
+    
+    def __init__(self, hand_label):
+        """
+        Constructs all the necessary attributes for the HandRecog object.
+
+        Parameters
+        ----------
+            finger : int
+                Represent gesture corresponding to Enum 'Gest',
+                stores computed gesture for current frame.
+            ori_gesture : int
+                Represent gesture corresponding to Enum 'Gest',
+                stores gesture being used.
+            prev_gesture : int
+                Represent gesture corresponding to Enum 'Gest',
+                stores gesture computed for previous frame.
+            frame_count : int
+                total no. of frames since 'ori_gesture' is updated.
+            hand_result : Object
+                Landmarks obtained from mediapipe.
+            hand_label : int
+                Represents multi-handedness corresponding to Enum 'HLabel'.
+        """
+
+        self.finger = 0
+        self.ori_gesture = Gest.PALM
+        self.prev_gesture = Gest.PALM
+        self.frame_count = 0
+        self.hand_result = None
+        self.hand_label = hand_label
+    
+    def update_hand_result(self, hand_result):
+        self.hand_result = hand_result
+
+    def get_signed_dist(self, point):
+        """
+        returns signed euclidean distance between 'point'.
+
+        Parameters
+        ----------
+        point : list contaning two elements of type list/tuple which represents 
+            landmark point.
+        
+        Returns
+        -------
+        float
+        """
+        sign = -1
+        if self.hand_result.landmark[point[0]].y < self.hand_result.landmark[point[1]].y:
+            sign = 1
+        dist = (self.hand_result.landmark[point[0]].x - self.hand_result.landmark[point[1]].x)**2
+        dist += (self.hand_result.landmark[point[0]].y - self.hand_result.landmark[point[1]].y)**2
+        dist = math.sqrt(dist)
+        return dist*sign
+    
+    def get_dist(self, point):
+        """
+        returns euclidean distance between 'point'.
+
+        Parameters
+        ----------
+        point : list contaning two elements of type list/tuple which represents 
+            landmark point.
+        
+        Returns
+        -------
+        float
+        """
+        dist = (self.hand_result.landmark[point[0]].x - self.hand_result.landmark[point[1]].x)**2
+        dist += (self.hand_result.landmark[point[0]].y - self.hand_result.landmark[point[1]].y)**2
+        dist = math.sqrt(dist)
+        return dist
+    
+    def get_dz(self,point):
+        """
+        returns absolute difference on z-axis between 'point'.
+
+        Parameters
+        ----------
+        point : list contaning two elements of type list/tuple which represents 
+            landmark point.
+        
+        Returns
+        -------
+        float
+        """
+        return abs(self.hand_result.landmark[point[0]].z - self.hand_result.landmark[point[1]].z)
