79f16a10b117f9ec6864c764c2c5cdd318a9ca3b
Angela Ferrer <gelferrero01@gmail.com>
2024-02-09 19:54:18 +0800

    Set up hand tracker

--- final_project.py
+++ final_project.py
@@ -9,3 +9,9 @@ from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 from google.protobuf.json_format import MessageToDict
 import screen_brightness_control as sbcontrol
+
+# Setting up hand tracker
+pyautogui.FAILSAFE = False
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
